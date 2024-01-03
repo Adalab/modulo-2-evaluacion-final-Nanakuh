6dac95c219749d91d9f9ed178435a07bbebcc091
Ana Gil <anagilrodrig@gmail.com>
2024-01-03 18:39:44 +0100

    Starting project

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,41 @@
 'use strict';
+const characters = [
+  {
+    name: "Cenicienta",
+    imageUrl: "https://static.wikia.nocookie.net/disney/images/d/d3/Vlcsnap-2015-05-06-23h04m15s601.png"
+  },
+  {
+    name: "Hercules",
+    imageUrl: "https://static.wikia.nocookie.net/disney/images/d/d3/Vlcsnap-2015-05-06-23h04m15s601.png"
+  },
+  {
+    name: "Pinoccho",
+    imageUrl: "https://static.wikia.nocookie.net/disney/images/d/d3/Vlcsnap-2015-05-06-23h04m15s601.png"
+  }
+]
 
-console.log('>> Ready :)');
+const resultList = document.querySelector('#result-list');
+
+/*characters.map((character) => {
+  let cardContainer = document.createElement('div');
+  let characterImg = document.createElement('img');
+  let characterName = document.createElement('p');
+  characterName.textContent = character.name;
+  characterImg.src = character.imageUrl;
+  cardContainer.appendChild(characterImg);
+  cardContainer.appendChild(characterName);
+  resultList.appendChild(cardContainer);
+})*/
+
+
+characters.map((character) => {
+  resultList.innerHTML += ` 
+  <div>
+    <img src=${character.imageUrl}/>
+    <p>${character.name}</p>
+  </div>
+  `
+})
+
+
+
