de8f29499dc8e49fbce1d82dfab354012f28b7b0
Ana Gil <anagilrodrig@gmail.com>
2024-01-09 19:33:51 +0100

    Add CreateCharacterCard function

--- src/js/main.js
+++ src/js/main.js
@@ -6,24 +6,36 @@ const searchBarInput = document.querySelector('.js_search-bar__input');
 const searchBarButton = document.querySelector('.js_search-bar__button');
 const favouritesList = [];
 
+
+function createCharacterCard(character) {
+
+  const card = document.createElement('div');
+  card.classList.add('card', 'js_card');
+  let characterImage = character.imageUrl
+  if (characterImage === undefined) {
+    characterImage = "https://via.placeholder.com/210x295/ffffff/555555/?text=Disney";
+  }
+  card.innerHTML = `
+    <img src="${characterImage}" alt="Character image">
+    <p>${character.name}</p>
+  `;
+  card.addEventListener('click', function (event) {
+    console.log('clavijo');
+  });
+  return card
+}
+
 fetch('//api.disneyapi.dev/character?pageSize=50')
   .then(response => {
     return response.json();
   })
   .then(characters => {
     characters.data.map((character) => {
-      let characterImage = character.imageUrl
-      if (characterImage === undefined) {
-        characterImage = "https://via.placeholder.com/210x295/ffffff/555555/?text=Disney";
-      }
 
-      cardsResult.innerHTML += ` 
-                  <div class="card js_card">
-                      <img src=${characterImage}/>
-                      <p>${character.name}</p>
-                  </div>
-              `;
-         
+      const characterCard = createCharacterCard(character);
+
+      cardsResult.appendChild(characterCard);
+
     });
   })
   .catch(error => console.log(error));
@@ -31,22 +43,17 @@ fetch('//api.disneyapi.dev/character?pageSize=50')
 searchBarButton.addEventListener('click', (event) => {
   event.preventDefault()
   const nameToSearch = searchBarInput.value;
-  console.log(nameToSearch);
   cardsResult.innerHTML = '';
   fetch(`//api.disneyapi.dev/character?name=${nameToSearch}`)
     .then(response => {
       return response.json();
     })
     .then(characters => {
-      console.log(characters.data.length);
-      if (characters.data.length > 0) {
+           if (characters.data.length > 0) {
         characters.data.map((character) => {
-          cardsResult.innerHTML += ` 
-                    <div class="card js_card">
-                        <img src=${character.imageUrl}/>
-                        <p>${character.name}</p>
-                    </div>
-                `;
+          const characterCard = createCharacterCard(character);
+
+          cardsResult.appendChild(characterCard);
         });
       }
       else {
@@ -62,47 +69,7 @@ searchBarButton.addEventListener('click', (event) => {
 
 })
 
-document.addEventListener("DOMContentLoaded", function() {
-  const cards = document.querySelectorAll(".js__card");
-  for (const card of cards) {
-      card.addEventListener("click", function() {
-          console.log("Tarjeta clickeada!");
-      });
-  }
-});
-/*const cards = document.querySelectorAll(".js_card");
-// Función que se ejecutará cuando se haga clic en la tarjeta
-function onCardClick() {
-    console.log("Tarjeta clickeada!");
-}
 
-// Recorrer todos los elementos y añadir un event listener a cada uno
-for (const card of cards) {
-    card.addEventListener("click", onCardClick);
-}*/
-
-
-
-
-
-//DATOS
-
-
-
-// FUNCIONES
-/*function renderOne(cardData) {
-  cardsResult.innerHTML += `
-
-`
-}*/
-// FUNCIONES DE EVENTOS (HANDLER)
-// EVENTOS
-
-/*cardsResult.addEventListener('click', (event) => {
-  console.log('Click');
-})*/
-// CÓDIGO CUANDO CARGA LA PAGINA
-// renderOne();
 
 
 
